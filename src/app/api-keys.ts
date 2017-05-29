45593234a7b7b9ed9cc4b3f50169ce1bfd7433c5
Chance Magno <chancemagno@gmail.com>
2017-05-29 10:14:36 -0700

    Delete api-keys.ts

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export var masterFirebaseConfig= {
-  apiKey: "YourApiKey",
-  authDomain: "YourAuthDomain",
-  databaseURL: "YourDatabaseURL",
-  projectId: "YourProjectID",
-  storageBucket: "YourStorageBucket",
-  messagingSenderId: "YourMessagingSenderId"
-  };
